cc7c9d967e90e45a6f72265310e8bb716b7f25d4
kubil6y <lieqb2@gmail.com>
2024-05-29 20:47:19 +0300

    remove data folder

--- components/cities.tsx
+++ components/cities.tsx
@@ -1,11 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
-import { citiesData } from "@/data/cities";
+
+const citiesData = [
+    {
+        id: 0,
+        name: "Ankara",
+        imageSrc: "/assets/cities/ankara.webp",
+        href: "/cities/ankara",
+    },
+    {
+        id: 1,
+        name: "Antalya",
+        imageSrc: "/assets/cities/antalya.webp",
+        href: "/cities/antalya",
+    },
+    {
+        id: 2,
+        name: "Bursa",
+        imageSrc: "/assets/cities/bursa.webp",
+        href: "/cities/bursa",
+    },
+    {
+        id: 3,
+        name: "Istanbul",
+        imageSrc: "/assets/cities/istanbul.webp",
+        href: "/cities/istanbul",
+    },
+    {
+        id: 4,
+        name: "Izmir",
+        imageSrc: "/assets/cities/izmir.webp",
+        href: "/cities/izmir",
+    },
+];
 
 export const Cities = () => {
     return (
         <div className="container space-y-8 mb-8">
-            <h3 className="text-3xl font-semibold">We are in every city of Türkiye!</h3>
+            <h3 className="text-3xl font-semibold">
+                We are in every city of Türkiye!
+            </h3>
 
             <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
                 {citiesData.map((city) => (
@@ -34,9 +68,7 @@ const CityCard = ({ name, imageSrc, href }: CityCardProps) => {
             className="group relative h-[240px] overflow-hidden rounded-xl shadow-lg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
         >
             <div className="absolute bottom-4 left-4 z-10 rounded-lg bg-white px-4 py-2 shadow-xl transition group-focus:outline-none group-focus:ring-2 group-focus:ring-ring group-focus:ring-offset-2">
-                <p className="capitalize text-primary">
-                    {name}
-                </p>
+                <p className="capitalize text-primary">{name}</p>
             </div>
             <Image
                 src={imageSrc}
